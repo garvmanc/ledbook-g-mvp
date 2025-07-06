fdf10773adc34714ae94eff4c8dba43fa551f9dc
Garv <garvmanchanda2005@gmail.com>
2025-07-06 23:11:42 +0530

    Route usage and db connection

--- server.js
+++ server.js
@@ -12,3 +12,20 @@ const productRoutes = require('./routes/product');
 const invoiceRoutes = require('./routes/invoice');
 const purchaseRoutes = require('./routes/purchase');
 const userRoutes = require('./routes/user');
+
+app.use('/api/customers', customerRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/invoices', invoiceRoutes);
+app.use('/api/purchases', purchaseRoutes);
+app.use('/api/users', userRoutes);
+
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MONGODB Connected'))
+.catch((err) => console.error('MONGODB connection error', err));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
