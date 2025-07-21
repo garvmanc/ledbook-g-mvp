dfce36034ae67890fc62a44e23c9bb6caa721220
Garv <garvmanchanda2005@gmail.com>
2025-07-21 23:59:21 +0530

    Journal Schema Update

--- models/Journal.js
+++ models/Journal.js
@@ -21,5 +21,8 @@ const JournalSchema = new mongoose.Schema({
             debit: { type: Number, default: 0 },
             credit: { type: Number, default: 0 }
         }
-    ]
+    ],
+    date: {
+        
+    }
 })
