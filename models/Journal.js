1019a00bbf904b791f254c7349aa2928b62e49bd
Garv <garvmanchanda2005@gmail.com>
2025-07-21 23:55:58 +0530

    Journal Schema Update

--- models/Journal.js
+++ models/Journal.js
@@ -4,4 +4,7 @@ const JournalSchema = new mongoose.Schema({
     ref: {
         type: mongoose.Schema.Types.ObjectId,
     },
+     refType: {
+    type: String, // 'Invoice', 'Purchase', etc.
+  },
 })
